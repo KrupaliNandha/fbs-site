27a29c7a764c9ba3e7c1e8ef8c373c48526dedbb
test-codegrin <testcodegrin@gmail.com>
2026-06-04 02:30:59 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/_next/", "/api/"],
+        disallow: ["/api/"],
       },
     ],
     sitemap: absoluteUrl("/sitemap.xml", baseUrl),
